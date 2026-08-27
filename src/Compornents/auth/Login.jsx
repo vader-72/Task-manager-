c5df2d022e4f514de69b1554e3b3103cef857ad5
Muhammad Sadiq <sadiqmohammad414@gmail.com>
2026-08-27 20:23:08 +0530

    made so much

--- src/Compornents/auth/Login.jsx
+++ src/Compornents/auth/Login.jsx
@@ -19,7 +19,8 @@ const Login = ({handlelogin}) => {
     
   return (
     <div className='flex h-screen w-screen items-center justify-center'>
-       <div className='border-2 rounded-3xl border-blue-200 p-30 '>
+       <div className='border-2 rounded-3xl border-gray-300 bg-zinc-50 bg-transparent p-20 hover:scale-105 transition duration-100 ease-in-out'>
+        <div className ='flex items-center justify-center bg-blue-600 mb-10 rounded-xl '><h1 className ='text-4xl text-gray-100 font-semibold p-5 hover:scale-105 transition duration-300 ease-in-out'>Login</h1></div>
         <form onSubmit={(e) => {
             submitHandler(e)
         }} className='flex flex-col items-center justify-center '>
@@ -29,16 +30,23 @@ const Login = ({handlelogin}) => {
             onChange={(e) =>{
                 setEmail(e.target.value)
             }}
-            required className=' text-white border-blue-200  outline-none border-2 bg-transparent rounded-full py-3 px-5 placeholder:text-gray-400'type="email" placeholder="enter your email"/>
+            required className=' text-black border-gray-300  outline-none border-2  rounded-2xl py-3 px-5 placeholder:text-gray-400' type="email" placeholder="enter your email"/>
             <input
             value={password} 
             onChange={(e) =>{
                 setPassword(e.target.value)
             }}
-            required className='text-white mt-5 border-blue-200 outline-none border-2  rounded-full py-3 px-5 placeholder:text-gray-400' type="password" placeholder="enter your password" />
+            required className='text-black mt-5 border-gray-300 outline-none border-2  rounded-2xl py-3 px-5 placeholder:text-gray-400' type="password" placeholder="enter your password" />
+            <a className ='mt-2 text-gray-500' href=''>forgot password?</a>
+            
 
-            <button className='mt-5 rounded-3xl hover:scale-105 transition duration-300 ease-in-out px-5 cursor-pointer py-3 text-black-500 border-none outline-none bg-blue-500 hover:bg-blue-400 font-semibold '>Log in</button>
+            <button className='text-gray-100 mt-5 rounded-2xl hover:scale-105 transition duration-300 ease-in-out px-5 cursor-pointer py-3 text-black-500 border-none outline-none bg-blue-600 hover:bg-blue-700 font-semibold '>Log in</button>
         </form>
+        
+        </div>
+        <div className ='flex w-100 h-125 rounded-3xl items-center justify-center flex-col ml-10 hover:scale-105 transition duration-300 ease-in-out bg-zinc-50 border-2  border-gray-300'>
+            <p className ='text-4xl font-semibold p-5 '>Task manager</p>
+            <img className ='rounded-2xl w-50 h-50'src='./src/assets/sadiq.png'/>
         </div>
     </div>
   )
