f282ff0c08b3733f9fe45d71cc5f1a190ae2fbb7
Muhammad Sadiq <sadiqmohammad414@gmail.com>
2026-08-28 00:18:04 +0530

    updated few things

--- src/Compornents/other/Header.jsx
+++ src/Compornents/other/Header.jsx
@@ -7,9 +7,9 @@ const logOutUser = () => {
 }
 const Header = ({data}) => {
   return (
-    <div className='mt-2 flex items-end justify-between '>
+    <div className='mt-2 flex items-end justify-between  '>
         <h1 className=' text-2xl font-medium'>Hello<br/><span className ='text-3xl font-semibold'>{data.name}</span> </h1>
-        <button onClick={logOutUser} className='bg-red-500 rounded-full px-5 py-3 text-white '>Log Out</button>
+        <button onClick={logOutUser} className='bg-red-900 hover:bg-red-950 rounded-2xl px-5 py-3 text-white mr-5'>Log Out</button>
     </div>
   )
 }
--- src/Compornents/other/TaskListNumbers.jsx
+++ src/Compornents/other/TaskListNumbers.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 
 const TaskListNumbers = ({data}) => {
   return (
-    <div className='flex gap-5 mt-10 justify-between '>
-      <div className ='rounded-xl py-6 px-9 w-[45%] bg-violet-400 '>
+    <div className='flex gap-5 mt-10 items-center justify-between  '>
+      <div className ='rounded-xl py-6 px-9 w-[45%] bg-sky-400 hover:scale-105 duration-500 transition ease-in-out'>
         <h2 className='text-3xl font-semibold'>{data.taskNumbers.newTask}</h2>
         <h3 className='text-xl font-medium'>New Task</h3>
       </div>
-      <div className ='rounded-xl py-6 px-9 w-[45%] bg-blue-400 '>
+      <div className ='rounded-xl py-6 px-9 w-[45%] bg-emerald-500 hover:scale-105 duration-500 transition ease-in-out'>
         <h2 className='text-3xl font-semibold'>{data.taskNumbers.completed}</h2>
         <h3 className='text-xl font-medium'>Completed Task</h3>
       </div>
-      <div className ='rounded-xl py-6 px-9 w-[45%] bg-green-400 '>
+      <div className ='rounded-xl py-6 px-9 w-[45%] bg-red-700 hover:scale-105 duration-500 transition ease-in-out'>
         <h2 className='text-3xl font-semibold'>{data.taskNumbers.active}</h2>
         <h3 className='text-xl font-medium'>Active Task</h3>
       </div>
-      <div className ='rounded-xl py-6 px-9 w-[45%] bg-yellow-400 '>
+      <div className ='rounded-xl py-6 px-9 w-[45%] hover:scale-105 duration-500 transition ease-in-out bg-zinc-400 '>
         <h2 className='text-3xl font-semibold'>{data.taskNumbers.failed}</h2>
         <h3 className='text-xl font-medium'>Failed Task</h3>
       </div>
